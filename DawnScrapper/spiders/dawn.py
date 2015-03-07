8dce58123380d85da4c3cf4545bb5133114edd17
aliirz <dejavu87@gmail.com>
2015-03-07 13:17:25 +0500

    added algorithm

--- DawnScrapper/spiders/dawn.py
+++ DawnScrapper/spiders/dawn.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class DawnSpider(scrapy.Spider):
     name = "dawn"
     allowed_domains = ["dawn.com"]
+    # start_urls = generate_urls()
     start_urls = (
         'http://www.dawn.com/newspaper/2015-01-07/',
         'http://www.dawn.com/newspaper/2015-03-03/',
@@ -14,11 +15,22 @@ class DawnSpider(scrapy.Spider):
 
     def parse(self, response):
         # print response
-        generate_urls()
+        # check response for <h2> <a>
+        # if contains tag 'election'
+            # add to CSV
 
     def generate_urls():
         startDate = datetime(2012,05,12)
         endDate = datetime(2013, 05, 12)
-
+        var urls = []
         for stop in stops(freq=delorean.DAILY, count = 10, timezone = 'US/Eastern', start = startDate, end = endDate):
             print stop
+
+            # var url = 'http://www.dawn.com/newspaper/'
+            # url = url + stop.str()
+            # urls.add(url)
+        # return urls
+
+
+
+        # install delorean, python-dateutil, scrapy
